6e02c4afe1eb6a4866e9ad8926083d05381209fc
Tomasz Wirkus <wirkus919@gmail.com>
2023-06-10 23:12:46 +0200

    Updated index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
+export { EncryptionMachine, codesArray } from "./dist/index";
+
 type Digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 export type OneTimeKey = `${Digit}${Digit}${Digit}`;
